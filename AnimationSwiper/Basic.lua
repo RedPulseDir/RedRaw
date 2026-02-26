f68a1ade1c8e8f7992d213a80e19b56d8522f10b
RedPulseDir <redpulseofficialsup@gmail.com>
2026-02-26 07:07:53 +0400

    Add files via upload

---
+++ AnimationSwiper/Basic.lua
@@ -0,0 +1,12 @@
+getgenv().emotesEnabled = true
+getgenv().animationsEnabled = true
+
+getgenv().emotes = { 
+    RakaiDance=135686271244127,
+    DieLit=124754178569693,
+    Michael=114873215991554
+}
+
+getgenv().animationPack = 'Vampire' 
+
+loadstring(game:HttpGet('https://redphubss.vercel.app/scripts/AnimationSwiper/OthFiles/Animation.lua'))()
